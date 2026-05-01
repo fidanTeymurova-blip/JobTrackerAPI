272f517131c4e00d68098e2dc4616689a683b952
Fidan Teymurova <fidanteymrva@gmail.com>
2026-05-01 17:44:34 +0400

    add gitignore cleanup

--- obj/Debug/net8.0/JobTrackerAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/JobTrackerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42bbb98709214a0869f1295faa76c292795b7d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
